d7e30ba964876b8276f0e1f8aa34fd4780977024
basyair7 <basyair.fathul@gmail.com>
2024-08-11 20:58:56 +0700

    modified include "spiffs" to "fs"

--- platformio/program_esp32/src/main.cpp
+++ platformio/program_esp32/src/main.cpp
@@ -5,7 +5,7 @@
 #include <main.h>
 #include <ADSConverter.h>
 #include <ArduinoJson.h>
-#include <spiffs>
+#include <fs>
 #include <programWiFi>
 #include <relaycontroller>
 #include <WebServer>
@@ -22,7 +22,7 @@ ADSInit* adsInit = new ADSInit;
 SEN_0257* sensor1 = new SEN_0257;
 SEN_0257* sensor2 = new SEN_0257;
 SEN_0189* sensor3 = new SEN_0189;
-SPIFFSProgram* spiffs = new SPIFFSProgram;
+FSProgram* fs = new FSProgram;
 RelayController* relaycontrol = new RelayController;
 ProgramWiFi* programWiFi = new ProgramWiFi;
 WebServer* webServer = new WebServer(3000);
@@ -163,7 +163,7 @@ public:
 
         TSbegin(115200);
         TSprintln(F("\nInitializing..."));
-        spiffs->setupFS();
+        fs->setupFS();
         bootBtn->begin();
         ledBoard->begin(LED_BUILTIN);
         relaycontrol->begin(1000);
